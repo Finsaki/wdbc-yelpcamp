796f60937a1381a61cb8db666a9031e0bbc2188b
akikop <aki.koppinen@metropolia.fi>
2023-05-22 14:17:31 +0300

    Styling with ejsmate and bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,15 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image:
+        "https://images.unsplash.com/photo-1416879595882-3373a0480b5b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0ODQzNTF8fHx8fHx8MTY4NDY5NzM5MQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sapiente voluptate dolorum beatae animi, at eius voluptates officiis cumque eum ab exercitationem quos dicta culpa. Odio natus eveniet tempore molestiae.",
+      price,
     });
     await camp.save();
   }
