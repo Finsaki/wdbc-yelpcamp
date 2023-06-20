1ec14928f299e2a3c377a8d1a5ad870e67dffb57
akikop <aki.koppinen@metropolia.fi>
2023-06-20 09:11:15 +0300

    Passport setup

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 const campgroundRoute = require("./routes/campgrounds");
 const reviewRoute = require("./routes/reviews");
@@ -42,6 +45,13 @@ const sessionConfig = {
 };
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+//enables persistent login sessions. app(session()) goes before this
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //this before route handlers
 app.use((req, res, next) => {
